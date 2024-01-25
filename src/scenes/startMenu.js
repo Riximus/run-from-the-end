ccadc9206330efc5a2469a8cf2ce6533eae62967
Riximus <riximuslol@gmail.com>
2024-01-25 23:45:29 +0100

    🚧 fixed looping music

--- src/scenes/startMenu.js
+++ src/scenes/startMenu.js
@@ -3,12 +3,18 @@ import UiManager from "../utils/UiManager";
 export default function startMenu(k) {
     const uiManager = new UiManager();
     uiManager.displayMainMenu();
-    const music = k.play("reggae", {
-        loop: true,
-        volume: 0.5,
-    })
+
+    let music
+    k.onClick(() => {
+        if (!music) {
+            music = k.play("reggae", {
+                loop: true,
+                volume: 0.5,
+            })
+        }
+    });
 
     k.onSceneLeave(() => {
-        music.stop();
+        if (music) music.stop();
     })
 }
